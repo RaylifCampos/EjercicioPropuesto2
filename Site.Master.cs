946d47d35eed6969b01b37003560c29d7b4806ac
unknown <andre.campos@ucsm.edu.pe>
2021-08-31 19:10:25 -0500

    Ejercicio Propuesto 2 completo

---
+++ Site.Master.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EjercicioPropuesto2
+{
+    public partial class SiteMaster : MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+               
+            
+        }
+        protected void MostrarGrafico_ServerClick(Object sender, EventArgs e)
+        {
+            Grafico.Src = "figura" + Lista.SelectedIndex.ToString() +".jpg";
+            Grafico.Alt = "Grafico Temperatura";
+            Grafico.Visible = true;
+        }
+    }
+}
